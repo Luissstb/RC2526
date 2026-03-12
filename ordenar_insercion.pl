405bf2d3ea6df4a30f3960a617a087c7bc7fe383
Luis <instagram.neutron083@slmails.com>
2026-03-12 09:59:13 +0100

    Create ordenar_insercion.pl

---
+++ ordenar_insercion.pl
@@ -0,0 +1,13 @@
+
+/*
+ordenar_insercion(Lista,E,ListaE). Lista unifica con una lista igual que ListaE pero ordenada y con el elemento E en la posicion correspondiente.
+
+[1,9,2,3]  7  --> [1,2,3,7,9]
+
+*/
+
+ordenar_insercion([],E,[E]).
+
+ordenar_insercion(Lista,E, ) :-
+
+
