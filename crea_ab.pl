ec7b31594193e2c8c577652df0e90330103943c3
Luis <instagram.neutron083@slmails.com>
2026-04-15 10:09:12 +0200

    Update crea_ab.pl

--- crea_ab.pl
+++ crea_ab.pl
@@ -12,28 +12,12 @@
 
 */
 
-crea_ab(ListaEtiquetas, ArbolBinario) :-
-    is_list(ListaEtiquetas), 
-    !,
-    construir_arbol_balanceado(ListaEtiquetas, ArbolBinario).
+crea_ab(ListaEtiquetas, ArbolBinario) : is_list(ListaEtiquetas), !, construir_arbol_balanceado(ListaEtiquetas, ArbolBinario).
 
-crea_ab(N, ArbolBinario) :-
-    integer(N),
-    N >= 0,
-    !,
-    (   N =:= 0
-    ->  Lista = []
-    ;   findall(X, between(1, N, X), Lista) 
-    ),
-    construir_arbol_balanceado(Lista, ArbolBinario).
+crea_ab(N, ArbolBinario) :- integer(N), N >= 0, !, (   N =:= 0->  Lista = [] ;   findall(X, between(1, N, X), Lista) ), construir_arbol_balanceado(Lista, ArbolBinario).
 
 construir_arbol_balanceado([], nil).
 
-construir_arbol_balanceado(Lista, arbol(Izq, Raiz, Der)) :-
-    Lista = [_|_],
-    length(Lista, Longitud),
-    Mitad is Longitud // 2,
-    length(ListaIzq, Mitad),
-    append(ListaIzq, [Raiz | ListaDer], Lista),
+construir_arbol_balanceado(Lista, arbol(Izq, Raiz, Der)) :- Lista = [_|_], length(Lista, Longitud), Mitad is Longitud // 2, length(ListaIzq, Mitad), append(ListaIzq, [Raiz | ListaDer], Lista),
     construir_arbol_balanceado(ListaIzq, Izq),
     construir_arbol_balanceado(ListaDer, Der).
